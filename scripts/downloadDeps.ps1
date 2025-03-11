323d7ab00c5353e90e008a911df54d357fe10c50
Mavaddat Javid <5055400+mavaddat@users.noreply.github.com>
2025-03-11 10:54:39 -0400

    Update jcgm downloadDeps.ps1

--- scripts/downloadDeps.ps1
+++ scripts/downloadDeps.ps1
@@ -1,38 +1,52 @@
 if (-not(Get-Command -Name mvn -ErrorAction SilentlyContinue)) {
-    if(Test-Path -Path "$env:LOCALAPPDATA\Programs\Maven\bin\")
-    {
+    if (Test-Path -Path "$env:LOCALAPPDATA\Programs\Maven\bin\") {
         $env:PATH += ";$env:LOCALAPPDATA\Programs\Maven\bin\"
-    }else{
-        Write-Error -Message "Cannot find Maven executable"
+    }
+    else {
+        Write-Error -Message 'Cannot find Maven executable'
         return
     }
 }
 Write-Verbose -Message 'Querying for latest JCGM-Core' -Verbose
-Invoke-RestMethod -Uri "https://api.github.com/repos/SwissAS/jcgm-core/releases/latest" -Verbose | Set-Variable -Name LatestJCGMCore
+Invoke-RestMethod -Uri 'https://api.github.com/repos/SwissAS/jcgm-core/releases/latest' -Verbose | Set-Variable -Name LatestJCGMCore
 $LatestJCGMCoreJars = $LatestJCGMCore.assets | Where-Object { $_.name -like '*.jar' }
 $jcgmLatestVersion = $LatestJCGMCoreJars.name -replace '.*?([\d\.]+).*', '$1' -replace '\.$' | Select-Object -First 1
 Write-Verbose -Message "Found $( $LatestJCGMCoreJars.Count ) assets for JCGM-Core version '$jcgmLatestVersion'" -Verbose
-[xml]$pom = Get-Content -Path .\pom.xml
+[xml]$pom = Get-Content -Path .\..\pom.xml
 $pomVersion = $pom.project.properties.'jcgm.version'
 Write-Verbose -Message "Current POM version of jcgm is '$pomVersion'" -Verbose
 $jcgmMvnPath = "$env:USERPROFILE\.m2\repository\net\sf\jcgm\core\jcgm-core\*\*.jar"
-if (-not (Test-Path -Path ($jcgmMvnPath  -replace '\\\*(?=\\\*\.jar)',"\$jcgmLatestVersion") -PathType Leaf -Verbose) -or $pomVersion -ne $jcgmLatestVersion) {
+if (-not (Test-Path -Path ($jcgmMvnPath -replace '\\\*(?=\\\*\.jar)', "\$jcgmLatestVersion") -PathType Leaf -Verbose) -or $pomVersion -ne $jcgmLatestVersion) {
     Write-Verbose -Message 'Updating project jcgm version' -Verbose
     $pom.project.properties.'jcgm.version' = $jcgmLatestVersion
-    $pom.Save(((Get-Item -Path '.\pom.xml').FullName))
+    $pom.Save(((Get-Item -Path '.\..\pom.xml').FullName))
     foreach ($JCGMCoreJar in $LatestJCGMCoreJars) {
-        Write-Verbose -Message "Downloading '$( $JCGMCoreJar.name )'"
-        $jarPath = "$env:TEMP\$( $JCGMCoreJar.name )"
+        Write-Verbose -Message "Downloading '$( $JCGMCoreJar.name )'" -Verbose
+        $jarPath = $JCGMCoreJar.name
         Invoke-WebRequest -Uri ($JCGMCoreJar.browser_download_url) -OutFile $jarPath -Verbose
         if ((Test-Path -Path $jarPath -PathType Leaf -Verbose) -and -not [string]::IsNullOrEmpty($jcgmLatestVersion)) {
-            Write-Verbose -Message "Installing '$( $JCGMCoreJar.name )'"
+            Write-Verbose -Message "Installing '$( $JCGMCoreJar.name )'" -Verbose
+            
+            # Define base arguments
+            $argsList = @(
+                'install:install-file',
+                "-Dfile=$jarPath",
+                "-DgroupId=net.sf.jcgm.core",
+                "-DartifactId=jcgm-core",
+                "-Dversion=$jcgmLatestVersion",
+                "-Dpackaging=jar"
+            )
+            # Add classifier if it is a sources jar
             if ($JCGMCoreJar.name -like '*-sources.jar') {
-                & mvn install:install-file -Dfile="$jarPath" -DgroupId='net.sf.jcgm.core' -DartifactId='jcgm-core' -Dversion="$jcgmLatestVersion" -Dpackaging='jar' -Dclassifier='sources'
-            } else {
-                & mvn install:install-file -Dfile="$jarPath" -DgroupId='net.sf.jcgm.core' -DartifactId='jcgm-core' -Dversion="$jcgmLatestVersion" -Dpackaging='jar'
+                $argsList += "-Dclassifier='sources'"
             }
-            if ($?) {
-                Write-Verbose -Message "Successfully installed '$( $JCGMCoreJar.name )'"
+
+            Write-Debug -Message "Arguments: $($argsList -join ' ')" -Debug
+            # Use Start-Process to call mvn
+            Start-Process -FilePath 'mvn' -ArgumentList $argsList -Wait -NoNewWindow
+        
+            if ($LASTEXITCODE -eq 0) {
+                Write-Verbose -Message "Successfully installed '$( $JCGMCoreJar.name )'" -Verbose
                 Remove-Item -Path $jarPath -Verbose
             }
             else {
